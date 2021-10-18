6cc173802e9113503862a5e8003ceab95397378d
opu34 <nurmdopu428@gmail.com>
2021-10-18 12:37:21 +0600

    dones

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Carousels from './carousel/Carousel';
 import Sopmeservice from './cart/Sopmeservice';
+import imgs from '../../image/info-block-5.webp'
 import './home.css'
 
 const Home = () => {
@@ -52,6 +53,19 @@ const Home = () => {
                          services?.map(service =><Sopmeservice service={service} key={service.id}/>)
                      }
                  </div>
+             </div>
+             <div className="row align-items-center mb-3">
+<div className="col-md-6">
+<img className="img-fluid" src={imgs} alt="" />
+</div>
+<div className="col-md-6">
+    <h1 className="fw-bold">about medicare</h1>
+    <p className="texts">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth. .</p>
+    <Link to ="/about">
+             <button className="enroll-btn apoitnment-btn"> <i class="far fa-calendar-check"></i> <span className="ms-2">about us</span></button>
+             </Link>
+</div>
+
              </div>
          </Container>
         </div>
